be7406ac42eab80478848b3db8d0ac425e0af137
IonCatana <catana.ion17@yahoo.it>
2022-03-28 15:05:24 +0200

    Creare e stampare il paragrafo

--- index.php
+++ index.php
@@ -3,20 +3,28 @@
 //Una parola da censurare viene passata dall’utente tramite parametro GET.
 //Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
 
-<?php 
+<?php
 
-
+// Variabili
+$speech = "La cosa più bella di un computer portatile e che, per quanta roba tu ci possa mettere dentro,
+           non diventa più grande o più pesante.";
+$lengthSpeech = strlen($speech);
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP BadWords</title>
 </head>
+
 <body>
-  <h1>Ciao</h1>
+  <div>
+    <p> <?php echo $speech; ?> </p>
+  </div>
 </body>
+
 </html>
